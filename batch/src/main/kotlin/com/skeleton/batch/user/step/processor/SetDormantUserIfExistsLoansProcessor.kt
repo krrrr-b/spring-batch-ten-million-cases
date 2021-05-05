eaabcf4958c07d183d9a88b0a7e3488ca70943b7
krr-b <82750844+krrrr-b@users.noreply.github.com>
2021-05-05 20:21:49 +0900

    Update SetDormantUserIfExistsLoansProcessor.kt

--- batch/src/main/kotlin/com/skeleton/batch/user/step/processor/SetDormantUserIfExistsLoansProcessor.kt
+++ batch/src/main/kotlin/com/skeleton/batch/user/step/processor/SetDormantUserIfExistsLoansProcessor.kt
@@ -4,9 +4,9 @@ import com.skeleton.batch.data.mysql.users.Users
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.stereotype.Component
 
-const val 여신_대출_존재하는_유저_휴면_처리_프로세서 = "setDormantUserIfExistsLoansProcessor"
+const val 여신_대출_존재하지_않는_유저_휴면_처리_프로세서 = "setDormantUserIfExistsLoansProcessor"
 
-@Component(여신_대출_존재하는_유저_휴면_처리_프로세서)
+@Component(여신_대출_존재하지_않는_유저_휴면_처리_프로세서)
 class SetDormantUserIfExistsLoansProcessor : ItemProcessor<Users, Users> {
     override fun process(entity: Users): Users? {
         return if (isExistsLoans(entity.id!!)) null
